3e70aa4604d7518bf0d9dbf40f2bb0a07b1b38fc
pedromiura <pedro-miura@hotmail.com>
2023-03-19 22:28:20 +0000

    [SHAW-9] Added styling to the modal

--- components/Modal/index.js
+++ components/Modal/index.js
@@ -3,6 +3,7 @@ import { useFormContext } from "../../context/form";
 import { Modal, ModalHeader, ModalBody } from 'reactstrap';
 import ImageComponent from "../ImageComponent"
 import Spinner from "../Spinner";
+import ModalStyle from '@/styles/Modal.module.css'
 
 /* 
 Documentation for integration with reactstrap: https://reactstrap.github.io/?path=/docs/components-modal--modal  
@@ -16,11 +17,24 @@ export default function MyModal(props) {
   }
 
   return (
-    <Modal size="lg" isOpen={props.isOpen} toggle={closeModal}>
+    <Modal size="lg" centered isOpen={props.isOpen} toggle={closeModal}>
       <ModalHeader toggle={closeModal}>Card</ModalHeader>
       <ModalBody>
-        <Suspense fallback={<Spinner/>}> {/* Built in react feature to manage loading times */}
-          <ImageComponent type="thumb" width="500" height="500"/>
+        <Suspense fallback={<Spinner />}> {/* Built in react feature to manage loading times */}
+          <div className={`container`}>
+            <div className={`row justify-content-around`}>
+              <div className={`col-xs-auto p-3 ${ModalStyle.image}`}>
+                <ImageComponent type="thumb" />
+              </div>
+              <div className="col-md-auto p-3">
+                <label for="name">Name:</label>
+                <input type="text" id="name" name="name" value={form.name} className={ModalStyle.input} readOnly/>
+                <p/>
+                <label for="surname">Surname:</label>
+                <input type="text" id="surname" name="surname" value={form.surname} className={ModalStyle.input} readOnly/>
+              </div>
+            </div>
+          </div>
         </Suspense>
       </ModalBody>
     </Modal>
